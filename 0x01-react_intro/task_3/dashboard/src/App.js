c21b2b0d1d7b9a98dfea021c0a6111dcf08628c2
dk-Gk <idkin19@gmail.com>
2022-08-02 23:25:17 +0000

    task_3

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+	  <>
+	  <header className='App-header'>
+	  <img src={logo} alt='logo' />
+	  <h1>School dashboard</h1>
+	  </header>
+	  <main role='main' className='App-body'>
+	  <p>Login to access the full dashboard</p>
+	  <label for='email'>Email</label>
+	  <input type='email' name='email' id='email' />
+	  <label for='password'>Password</label>
+	  <input type='password' name='password' id='password' />
+	          <button type='button'>OK</button>
+	  </main>
+	  <footer className='App-footer'>
+	  <p>Copyright 2020 - holberton School</p>
+	  </footer>
+	  </>
+  );
+}
+
+export default App;
